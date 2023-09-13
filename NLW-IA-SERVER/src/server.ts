211bdd16878a54f4713ca4c78484510ee0e6e1d4
MagalhaesDev <mateusmagalhaesemidio@gmail.com>
2023-09-13 19:34:10 -0300

    alteration paths

---
+++ NLW-IA-SERVER/src/server.ts
@@ -0,0 +1,16 @@
+import { fastify } from 'fastify';
+import { createTranscriptionRoute } from './routes/create-transcription';
+import { getAllPromptsRoute } from './routes/get-all-prompts';
+import { uploadVideoRoute } from './routes/upload-video';
+
+const app = fastify()
+
+app.register(getAllPromptsRoute);
+app.register(uploadVideoRoute);
+app.register(createTranscriptionRoute);
+
+app.listen({
+  port: 3333,
+}).then(() => {
+  console.log('HTTP Server Running!')
+})
